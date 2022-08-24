1996ffb7f8a33bd27d6ca741e7518eb572d8394f
nikoDev <nikolas090189@gmail.com>
2022-08-24 12:47:27 -0300

    base 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from 'react-hook-form'
+import { ErrorMessage } from '@hookform/error-message'
+import './App.css'
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      checkbox: [],
+      radio: '',
+      category: '',
+      message: '',
+    },
+  })
+  const onSubmit = (data: any) => console.log(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        {...register('firstName', { pattern: /^[\w]{8}$/, required: true })}
+        placeholder="First name"
+      />
+      <ErrorMessage errors={errors} message={'Message error'} name="firstName" />
+
+      <input {...register('lastName', { minLength: 2 })} placeholder="Last name" />
+
+      <select {...register('category', { required: true })}>
+        <option value="">Select...</option>
+        <option value="A">Category A</option>
+        <option value="B">Category B</option>
+      </select>
+      <ErrorMessage errors={errors} name="category" />
+
+      <textarea {...register('message', { maxLength: 200 })} placeholder="message" />
+
+      <input type="submit" />
+    </form>
+  )
 }
 
-export default App;
+export default App
